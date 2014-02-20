8e2e024e98ff949fc9099b2de5848018321afb82
tappan-at-git <erichtappan@gmail.com>
2014-02-20 12:10:30 -0500

    Fixed regex.c. Filled in center.c. We bounce.

--- exam1/center.c
+++ exam1/center.c
@@ -18,22 +18,57 @@ License: Creative Commons Attribution-ShareAlike 3.0
 
 char *center(char *s, int n, char fillchar)
 {
-    // FILL THIS IN
-    //    return NULL;
+    int len = strlen(s);        // Don't loop through the string more than you 
+                                // need to.
+    if (n < len) {
+        fprintf(stderr, "could not center %s with %c in a length %i string: n \
+must be longer than strlen(s)!", s, fillchar, n);
+        exit(1);
+    }
+    char *result = malloc(sizeof(char)*n);
+    char *buf = result;         // One pointer to return, and one to advance.
+    
+    int leftfill = (n-len)/2; // left fill may be 1 less than right fill to 
+                              // deal with odd length strings.
+    int i = 0;
+    // the first leftfill characters in *result will be filler
+    while (i<leftfill) {
+        *(buf++) = fillchar;    // increment but look at initial pointer
+        i++;
+    }
+    // the centered string, which we know to be length len
+    while (i<(leftfill+len)) {
+        *(buf++) = *(s++);
+        i++;
+    }
+    // the remaining characters up to the nth are filler
+    while (i<n) {
+        *(buf++) = fillchar;
+        i++;
+    }
+    return result;
 }
 
 
 int main (int argc, char *argv[])
 {
+    // ..............................
+    // ...........Software...........
     char *s = center("Software", 30, '.');
     printf("%s\n", s);
-
+    
+    // -------------------------------
+    // ------------Systems------------
     char *s2 = center("Systems", 31, '-');
     printf("%s\n", s2);
 
+    // ********************************
+    // **********Spring 2014***********
     char *s3 = center("Spring 2014", 32, '*');
     printf("%s\n", s3);
 
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // ^^^^^^^^^^^^^Exam 1^^^^^^^^^^^^^^
     char *s4 = center("Exam 1", 33, '^');
     printf("%s\n", s4);
 
--- exam1/regex.c
+++ exam1/regex.c
@@ -10,7 +10,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <string.h>
 #include <regex.h>
 
-#define NUM_TRACKS 5;
+#define NUM_TRACKS 5   // (4) Damn your ;!
 
 char tracks[][80] = {
     "So What",
@@ -42,41 +42,52 @@ void find_track_regex(char *pattern)
 {
     int i, ret;
     regex_t regex;
-    char *msgbuf;
-
-    ret = regcomp(&regex, pattern, REG_EXTENDED || REG_NOSUB);
+    int msglen = 30;
+    char *msgbuf = malloc(sizeof(char)*msglen);       // (2) No size allocated 
+                                                  // originally.
+    // int regcomp returns 0 on success and an error code otherwise.
+    ret = regcomp(&regex, pattern, REG_EXTENDED | REG_NOSUB);  // (1) Should be 
+                                                               // bitwise OR, |.
+                                                               // Was ||.
     if (ret) {
-        fprintf(stderr, "Could not compile regex\n");
-        exit(1);
+        fprintf(stderr, "Could not compile regex\n");   // Could be more useful.
+        exit(1);        // exits on error
     }
 
     for (i=0; i<NUM_TRACKS; i++) {
+        // int regexec returns 0 on match, REG_NOMATCH, or an error value
         ret = regexec(&regex, tracks[i], 0, NULL, 0);
 	if (!ret) {
 	    printf("Track %i: '%s'\n", i, tracks[i]);
 	} else if (ret == REG_NOMATCH) {
 	    continue;
 	} else {
-           regerror(ret, &regex, msgbuf, sizeof(msgbuf));
+           regerror(ret, &regex, msgbuf, msglen); // (3) sizeof doesn't 
+                                                        // work that way!
            fprintf(stderr, "Regex match failed: %s\n", msgbuf);
-	   exit(1);
+	   exit(1);     // exits on error
 	}
     }
 
     /* I'm not sure this is necessary, but it's possible that if you
        let regex go out of scope without running regfree, it leaks
        (that is, leaves some allocated memory unfreed). */
-    regfree(regex);
+    regfree(&regex);    // (5) Necessary or not, use a pointer! 
+    free(msgbuf);
 }
 
 
 int main (int argc, char *argv[])
 {
-    char *target = 'F';
-    char *pattern = "Fr.*Fr.*";
+    // (6) 'F' is a character, not a string. Good luck with that one.
+    char *target = "F";         // Finds "Freddie Freeloader" and "Flamenco 
+                                // Sketches"
+    char *pattern = "Fr.*Fr.*"; // Looks for "Fr"+0 or more other 
+                                // characters+"Fr"+ 0 or more other character.
+                                // Finds "Freddie Freeloader".
 
-    find_track(target);
-    find_track_regex(pattern);
+    find_track(target);         // No return asked for or specified.
+    find_track_regex(pattern);  // No return asked for or specified.
 
     return 0;
 }
