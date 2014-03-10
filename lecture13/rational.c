5eda24d732e52f4d33144d6cfb762f8ee775dc09
etappan <erichtappan@gmail.com>
2014-03-10 11:22:07 -0400

    completed rational.c

--- lecture13/rational.c
+++ lecture13/rational.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 
+// a rational number is a fraction in the form (float) (int numer)/(int denom)
 typedef struct {
     int numer;
     int denom;
@@ -12,20 +13,29 @@ typedef struct {
 // Returns a new Rational with the given numer and denom.
 // If unable to allocate, prints an error message and exits.
 Rational *make_rational(int numer, int denom) {
-    // FILL THIS IN
-    return NULL;
+    Rational *rat = (Rational *) malloc(sizeof(Rational));
+    if (rat == NULL) {
+      printf("Malloc failed. Could not make %d/%d.",numer,denom);
+      exit(1);
+    }
+    rat->numer = numer;
+    rat->denom = denom;
+    return rat;
 }
 
 // Computes a floating-point approximation of a Rational.
 double rational_to_double(Rational *rational) {
-    // FILL THIS IN
-    return 0.0;
+    double numer = rational->numer;
+    double denom = rational->denom;
+    return (numer/denom);
 }
 
-// Multiplies two rational numbers; returns a new Rational.
+// Multiplies two rational numbers; returns a new Rational. Does not simplify.
 Rational *mult_rational(Rational *r1, Rational *r2) {
-    // FILL THIS IN
-    return NULL;
+    int numer = (r1->numer)*(r2->numer);
+    int denom = (r1->denom)*(r2->denom);
+    Rational *new_rat = make_rational(numer,denom);
+    return new_rat;
 }
 
 // Frees a Rational.
@@ -41,12 +51,15 @@ void print_rational(Rational *rational) {
 int main(void)
 {
     Rational *rational = make_rational(3, 7);
+    printf("3 / 7 = ");
     print_rational(rational);
 
     double d = rational_to_double(rational);
+    printf("0.428571 = ");
     printf("%lf\n", d);
 
     Rational *square = mult_rational(rational, rational);
+    printf("(3/7)^2 = 9/49 = ");
     print_rational(square);
 	
     free_rational(rational);
